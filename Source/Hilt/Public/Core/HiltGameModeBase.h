3e4a3e0c11c9d60d5cadccafbabc91ca0d7fc453
Sebastian Zapart <sebastian.zapart@gmail.com>
2024-02-28 15:54:20 +0100

    Project Init

---
+++ Source/Hilt/Public/Core/HiltGameModeBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HiltGameModeBase.generated.h"
+
+UCLASS()
+class HILT_API AHiltGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
