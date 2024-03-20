b26d6e7f9eb2148a5fb957e5c9ac68ea8d2ff0b8
brukernavnD <90253215+brukernavnD@users.noreply.github.com>
2024-03-20 15:26:44 +0100

    moving over code

--- Source/Hilt/Hilt.Build.cs
+++ Source/Hilt/Hilt.Build.cs
@@ -10,7 +10,7 @@ public class Hilt : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "NavigationSystem" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "NavigationSystem"});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
