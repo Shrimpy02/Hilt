bd2d5f8c01eaaba1a340fd2dbf20ac3250d1434b
brukernavnD <90253215+brukernavnD@users.noreply.github.com>
2024-10-29 17:05:12 +0100

    more bugfixing

--- Source/Hilt/Public/Core/HiltGameModeBase.h
+++ Source/Hilt/Public/Core/HiltGameModeBase.h
@@ -67,6 +67,8 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 	virtual void RestartLevel();
+
+	UFUNCTION(BlueprintCallable)
 	void RestartCooldownComplete();
 
 	void ShowAllStreamingLevels();
